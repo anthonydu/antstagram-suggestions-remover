16fc4c9f7876e05d1493c853e1f157a8564016fc
Anthony Du <github@anthonydu.com>
2023-10-07 00:16:08 -0700

    Added scrolling redirects

--- content.js
+++ content.js
@@ -12,15 +12,27 @@ observer.observe(document, {
 
 function handle() {
   var lastScrollPosition;
-  document.addEventListener("scroll", () => {
+  var hasRedirected = false;
+  listener = document.addEventListener("scroll", () => {
     if (window.location.pathname == "/" && !window.location.href.includes("?")) {
       if (document.querySelector('a[href="/?variant=past_posts"]').getBoundingClientRect().bottom + 50 < window.innerHeight) {
         console.log("over! over!", window.scrollY);
-
-        window.scrollTo({ top: lastScrollPosition })
+        window.scrollTo({ top: lastScrollPosition });
+        // document.getElementById("mount_0_0_rm").style.height = window.screenTop + window.innerHeight + "px";
+        // document.getElementById("mount_0_0_rm").style.overflow = "hidden";
+        if (!hasRedirected) {
+          window.location.href += "?variant=past_posts";
+          hasRedirected = true;
+        }
       } else {
         lastScrollPosition = window.scrollY;
       }
     }
   });
 }
+
+document.addEventListener("scroll", () => {
+  if (window.location.href.includes("?variant=past_posts") && window.scrollY == 0) {
+    window.location.href = window.location.href.replace("?variant=past_posts", "");
+  }
+});
